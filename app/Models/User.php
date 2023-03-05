d804c1d78cf2a06ded1f75cd79bd940408d10051
Ryan Kadri <ryankadri.dev@gmail.com>
2023-03-05 16:31:00 +0100

    :globe_with_meridians: One To One learners users

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -41,4 +42,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the learner associated with the user account
+     */
+    public function learner(): BelongsTo {
+        return $this->belongsTo(Learner::class);
+    }
 }
